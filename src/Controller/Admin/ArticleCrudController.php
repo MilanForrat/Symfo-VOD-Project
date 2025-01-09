45064fb94c1d655b57672cde433a9dfcbc8d17e5
milan.forrat <milan.forrat@gmail.com>
2025-01-09 16:13:33 +0100

    feat: dynamic content on homepage

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -7,6 +7,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+
+
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -15,17 +19,37 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
+
     
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id')->hideOnForm(),
-            TextField::new('title'),
-            TextField::new('subtitle'),
-            TextEditorField::new('textContent'),
-            TextField::new('image'),
-            TextField::new('slug'),
+        $fields = [
+            // personnalisation du système d'upload d'image
+            // "image" est le nom du champ de l'entité et "Image" est le nom qui sera affiché dans easyAdmin
+            ImageField::new('image', 'Image')
+                // répertoire dans lequel seront enregistrées les images
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads')
+                // on renomme l'image sur le serveur pour éviter le duplicata (nom unique à chaque image)
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                // rendre obligatoire l'upload d'image
+                ->setRequired(false),
         ];
+        // permet de construire le slug à partir du name
+        $slug=SlugField::new('slug')->setTargetFieldName('title');
+
+        $title=TextField::new('title', "Titre");
+
+        $subtitle = TextField::new('subtitle', "Sous-titre");
+
+        $textContent = TextEditorField::new('textContent', "Contenu de l'article");
+
+        $fields[]=$slug;
+        $fields[]=$title;
+        $fields[]=$subtitle;
+        $fields[]=$textContent;
+        
+        return $fields;
     }
     
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+// appel de doctrine pour communiquer avec la BDD
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Article;
+use App\Entity\Video;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +14,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+
+        $video = $entityManager->getRepository(Video::class)->findAll();
+        // débugger 
+        // var_dump($video); die;
+        $article = $entityManager->getRepository(Article::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'toto' => 'test',
+            'article' => $article,
+            'video' => $video,
         ]);
     }
 }
